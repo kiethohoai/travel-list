70f493f1268547189831d40f5e00693129f56b65
kiethohoai <kietho.060193@gmail.com>
2024-06-26 14:28:14 +0700

    018 Controlled Elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
   { id: 3, description: "Chager", quantity: 1, packed: false },
 ];
 
+// App
 export default function App() {
   return (
     <div className="app">
@@ -15,20 +18,31 @@ export default function App() {
   );
 }
 
+// Logo
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 
+// Form
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log("🚀CHECK  newItem =", newItem);
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
           return (
             <option value={num} key={num}>
@@ -37,12 +51,18 @@ function Form() {
           );
         })}
       </select>
-      <input type="text" placeholder="Item..." name="" id="" />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
 }
 
+// PackingList
 function PackingList() {
   return (
     <div className="list">
@@ -55,6 +75,7 @@ function PackingList() {
   );
 }
 
+// Item
 function Item({ item }) {
   return (
     <li>
@@ -66,6 +87,7 @@ function Item({ item }) {
   );
 }
 
+// Stats
 function Stats() {
   return (
     <footer className="stats">
