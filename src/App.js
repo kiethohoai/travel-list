23f5e5b444e887669e1033d0902df07f15fc6cae
kiethohoai <kietho.060193@gmail.com>
2024-06-26 21:35:16 +0700

    Updating an Item Complex Immutable Data Operation

--- src/App.js
+++ src/App.js
@@ -21,11 +21,23 @@ export default function App() {
     setItems((items) => items.filter((item) => item.id !== id));
   }
 
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item,
+      ),
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form handleAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
@@ -76,7 +88,7 @@ function Form({ handleAddItems }) {
 }
 
 // PackingList
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
@@ -84,7 +96,12 @@ function PackingList({ items, onDeleteItem }) {
           items.length > 0 &&
           items.map((item) => {
             return (
-              <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+              <Item
+                item={item}
+                key={item.id}
+                onDeleteItem={onDeleteItem}
+                onToggleItem={onToggleItem}
+              />
             );
           })}
       </ul>
@@ -93,9 +110,14 @@ function PackingList({ items, onDeleteItem }) {
 }
 
 // Item
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
